707fd92f6d601f3ebe0d6599cf04e208b079a34f
Brian Williams <brcrwilliams@gmail.com>
2017-08-31 23:13:40 -0500

    Add comments to insertion2.rb

--- algorithms/sorting/insertion2.rb
+++ algorithms/sorting/insertion2.rb
@@ -1,19 +1,21 @@
 # https://www.hackerrank.com/challenges/insertionsort2
 
 def doSort(arr, position)
-  num = arr[position]
-  arr.delete_at(position)
+  num = arr[position] # Get the number to be sorted
+  arr.delete_at(position) # Remove it from the array
   arr.each_with_index.map do |val, i|
-    return arr.insert(i, num) if num <= val
+    return arr.insert(i, num) if num <= val # Find where it goes and insert
   end
 end
 
+# Get input from stdin
 n = gets.strip.to_i
 arr = gets.strip.split(' ').map(&:to_i)
 
-(n-1).times do |i|
+# Loop through the array checking if n < n+1. If it's not, do insertion sort on n+1
+(n - 1).times do |i|
   if arr[i] > arr[i + 1]
     arr = doSort(arr, i + 1)
   end
-  puts arr.join(' ')
+  puts arr.join(' ') # The challenge wants us to print each iteration
 end
